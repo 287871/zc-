58c7d7a9cef58e9013732a4d3214064036849d57
287871 <66677129+287871@users.noreply.github.com>
2021-01-22 09:21:59 +0800

    Update zc-.lua

--- zc-.lua
+++ zc-.lua
@@ -31,14 +31,14 @@ end
 -- Update
 http.Get(version_url, function(content)
 	if version == string.gsub(content, "[\r\n]", "") then
-		print("[LuaLoader] is up to date")
+		print("[LuaLoader] 最新版本")
 	else
         local new_version = http.Get("https://raw.githubusercontent.com/287871/zc-/main/zc-.lua");
         local old = file.Open(GetScriptName(), "w")
         old:Write(new_version)
         old:Close()
 		
-		print("[LuaLoader] updated")
+		print("[LuaLoader] 请更新")
 		UnloadScript(GetScriptName())
 	end
 end)
